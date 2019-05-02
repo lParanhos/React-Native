c15f05920d9df6a822af7d6041e98eababe61097
lParanhos <leandro-paranhos@hotmail.com>
2019-05-01 23:47:44 -0300

    Adicionando casca principal

--- nubankClone/src/components/Header/styles.js
+++ nubankClone/src/components/Header/styles.js
@@ -1,4 +1,4 @@
-import styled from 'react-native';
+import styled  from 'styled-components/native';
 
 export const Container = styled.View`
     align-items: center;
--- nubankClone/src/pages/Main/index.js
+++ nubankClone/src/pages/Main/index.js
@@ -1,13 +1,36 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons'
 
-import Header from '~/components/Header'
-import { Container } from './style';
+
+import Header from '~/components/Header';
+import Tabs from '~/components/Tabs';
+import {
+  Container, Content, Card, CardContent, CardFooter, CardHeader,
+Title, Description, Annotation
+} from './style';
 
 export default function Main() {
   return (
-
     <Container >
       <Header />
+      <Content>
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#666" />
+            <Icon name="visibility-off" size={28} color="#666" />
+          </CardHeader>
+          <CardContent>
+            <Title>Saldo Disponível</Title>
+            <Description>R$ 197.611,65</Description>
+          </CardContent>
+          <CardFooter>
+            <Annotation>
+              Transferência de R$20,00 recebida de NoobMaster  hoje ás 06:01h
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
+      <Tabs />
     </Container>
   )
 }
