5603b3f9ae6d3381dced15452761d4d6c3189032
lParanhos <leandro-paranhos@hotmail.com>
2019-05-01 22:49:21 -0300

    Adicionando os componentes

---
+++ nubankClone/src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import logo from '~/assets/Nubank_Logo.png';
+import { Container, Top, Logo, Title } from './styles';
+
+export default function Header() {
+    return (
+        <Container>
+            <Top>
+                <Logo source={logo} />
+                <Title>Leandro</Title>
+            </Top>
+            <Icon name="keyboard-arrow-down" size={20} color="#FFF" />
+        </Container>
+    );
+}
---
+++ nubankClone/src/components/Tabs/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Container } from '../../pages/Main/style';
+
+export default function Tabs() {
+    return (
+        <Container />
+    );
+}
