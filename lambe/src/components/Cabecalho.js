000d034047a92b2349689a153eacc355cfcc3c84
lParanhos <leandro-paranhos@hotmail.com>
2019-05-14 15:34:48 -0300

    Adicionando nome do usuario no header

--- lambe/src/components/Cabecalho.js
+++ lambe/src/components/Cabecalho.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Gravatar } from 'react-native-gravatar';
 import { View, Text, Image, StyleSheet, Platform } from 'react-native';
 import icon from '../../assets/imgs/icon.png';
-export default class Header extends Component {
+class Header extends Component {
 
     render() {
+        const name = this.props.name || 'Visitante';
+        const gravatar = this.props.email ?
+            <Gravatar options={{ email: this.props.email, secure: true }}
+                style={styles.avatar} />
+            : null;
         return (
             <View style={styles.container}>
                 <View style={styles.rowContainer}>
                     <Image source={icon} style={styles.image} />
                     <Text style={styles.title}>Lamb Lamb</Text>
+                </View>
+                <View style={styles.userContainer}>
+                    <Text style={styles.user}>{name}</Text>
+                    {gravatar}
                 </View>
             </View>
         );
@@ -21,7 +32,9 @@ const styles = StyleSheet.create({
         padding: 10,
         borderBottomWidth: 1,
         borderColor: '#BBB',
-        width: '100%'
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between'
     },
     rowContainer: {
         flexDirection: 'row',
@@ -37,5 +50,27 @@ const styles = StyleSheet.create({
         fontFamily: 'shelter',
         height: 30,
         fontSize: 28
+    },
+    userContainer: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    user: {
+        fontSize: 20,
+        color: '#888'
+    },
+    avatar: {
+        width: 30,
+        height: 30,
+        marginLeft: 10
     }
 })
+
+const mapStateToProps = ({ user }) => ({
+
+    email: user.email,
+    name: user.name
+
+});
+
+export default connect(mapStateToProps, null)(Header);
